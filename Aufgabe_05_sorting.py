7b4dfdd9f02c18365dc8d95642c9fc86e2dce9ff
SimonBraumeister <133128648+SimonBraumeister@users.noreply.github.com>
2023-05-19 20:34:14 +0200

    Update Aufgabe_05_sorting.py
    läuft alles

--- Aufgabe_05_sorting.py
+++ Aufgabe_05_sorting.py
@@ -9,36 +9,36 @@ mix = [51, 0, "Q", "LISTE", 4.3, {"null" : 0, "eins" : 1, "zwei" : 2}, -1, "Tupe
 
 # START TODO ###################
 
+# Definiere die Sortierten Listen und Variablen
 ints = []
 doubles = []
 strings = []
 dicts = []
-summe = []
-# 
+summe = 0
+
 for val in mix:
    
-    if val is type(bool): # auf boolean prüfen
+    if type(val) is bool: # auf boolean prüfen
         break            # laut Aufgstellung Schleife abbrechen
-    elif val is type(int): # auf interger prüfen
-        ints = ints.append[val]    # zu ints[] hinzufügen
+        
+    elif type(val) is int: # auf interger prüfen
+       ints.append(val)    # zu ints hinzufügen
     
-    elif val is type(float): # auf doubles prüfen
-        doubles = doubles.append[val]
-        # zu doubles[] hinzufügen
+    elif type(val) is float: # auf doubles prüfen
+        doubles.append(val) # zu doubles hinzufügen
+        
+    elif type(val) is str: # auf strings prüfen
+        strings.append(val) # zu strings hinzufügen
+        
+    elif type(val) is dict: # auf dictionaries prüfen
+        dicts.append(val) # zu dicts hinzufügen
+        
+# alle strings und floats zusammenaddieren    
+merged = ints + doubles    # Listen aus floats und intergers zusammenfügen     
 
-    elif val is type(str): # auf strings prüfen
-        strings = strings.append[val]
-        # zu strings[] hinzufügen
-
-    elif val is type(dict): # auf dictionaries prüfen
-        dicts = dicts.append[val]
-        # zu dicts[] hinzufügen
+for i, index in enumerate(merged): 
+    summe += float(merged[i])      # merged in float umwandeln und der Summe hinzufügen für alle Werte in der Liste
     
-        # ... alle strings und floats zusammenaddieren
-
-    
-
-
 # END TODO #####################
 
 
